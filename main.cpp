0d72994282636c3819b2bbe556de3c894d481e0a
Rodrigo Castanon de Carvalho <rodrigo.castanon@poli.ufrj.br>
2020-10-09 16:01:04 -0300

    logica de endOfLine funcionando

--- main.cpp
+++ main.cpp
@@ -23,38 +23,41 @@ int main(){
   ifstream MyReadFile("teste.txt");
   while(getline(MyReadFile, myText)){
     string word = "";
-    for (int index = 0;index < myText.length() ; index++) {
+    for (int index = 0;index <= myText.length() ; index++) {
       int comparisonResult = strncmp(&myText[index], " ", 1);
-      if (comparisonResult != 0){
-        /* tolower( myText[index]); */
-        /* word = word +  myText[index]; */
+      int endOfLine = strncmp(&myText[index], "\0", 1);
+      if (comparisonResult != 0 && endOfLine != 0 ){
         word += tolower(myText[index]);
       }else {
         wordsArray.push_back(word);
-        grafo.addVertice(word);
+        if (ispunct(word.back())){
+          string s = word;
+          s.pop_back();
+          grafo.addVertice(s);
+        }else {
+          grafo.addVertice(word);
+        }
         if(wordsArray.size() > 1){
-          cout << "criação de vertice" << endl;
-          cout << "palavra 1: " << wordsArray.end()[-2] << endl;
-          cout << "palavra 2: " << wordsArray.end()[-1] << endl;
-          cout <<endl;
-          grafo.addAresta(wordsArray.end()[-2], wordsArray.end()[-1]);
+          if (ispunct(wordsArray.end()[-2].back())){
+          }else if(ispunct(wordsArray.end()[-1].back())){
+            string wordCopy = wordsArray.end()[-1];
+            wordCopy.pop_back();
+            grafo.addAresta(wordsArray.end()[-2], wordCopy);
+          }else{
+            grafo.addAresta(wordsArray.end()[-2], wordsArray.end()[-1]);
+          }
         }
         word = "";
       }
     }
-    wordsArray.push_back(word);
-    grafo.addVertice(word);
-    if(wordsArray.size() > 1){
-      grafo.addAresta(wordsArray.end()[-2], wordsArray.end()[-1]);
-    }
   }
   MyReadFile.close();
-  cout << graphVectors->size() << endl;
-  /* for (int i =0; i < graphVectors->size(); i++){ */
-  /*   cout << (*graphVectors)[i].getName() << " :" << (*graphVectors)[i].getWeight() << endl; */
-  /* } */
-  cout << "peso das arestas de: " << (*graphVectors)[0].getName()<<endl;
-  cout << "quantidade de arestas" << (*(*graphVectors)[0].getArestas()).size()<<endl;
+  cout <<"tamanho do array de vetores: " <<graphVectors->size() << endl;
+  for (int i =0; i < graphVectors->size(); i++){
+    cout << (*graphVectors)[i].getName() << " :" << (*graphVectors)[i].getWeight() << endl;
+  }
+  cout << "peso das arestas de: " << (*graphVectors)[1].getName()<<endl;
+  cout << "quantidade de arestas" << (*(*graphVectors)[1].getArestas()).size()<<endl;
   cout << "peso: "<<(*(*graphVectors)[0].getArestas())[0].getWeight() <<endl;
   cout << "segundo vertice:" <<(*(*graphVectors)[0].getArestas())[0].getEndVertice()->getName() <<endl;
   return 1;
